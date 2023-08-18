072bd8858da0418becc60f57b85bf84cc74496ff
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-17 20:30:24 -0500

    feat: :safety_vest: Implementacion de la Validacion a los Endpoints: RegistroEntrega
    Se modifica el archivo para incluir los Middlewares de validacion, validando la data de entrada en los metodos Post y Put y ademas en los endpoints que soliciten ID en el query o en el body se valida dicha data.

--- routes/CRegistroEntrega.js
+++ routes/CRegistroEntrega.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import {limitGColecciones, limitPColecciones, limitDColecciones} from '../middleware/limit.js';
-import bodyParser  from 'body-parser';
-import { Collection, ObjectId } from 'mongodb';
+import { proxyPostC } from "../middleware/proxyCrudColecciones.js";
+import {proxyQueryID, proxyBodyID} from "../middleware/proxyUniversal.js"
+import errorcontroller from "../middleware/ErroresMongo.js";
 import { con } from '../db/atlas.js';
 
 const AppRegistroEntrega = Router();
@@ -15,49 +16,51 @@ AppRegistroEntrega.get('/GetRegistroEntrega', limitGColecciones(), async (req, r
 
 })
 
-AppRegistroEntrega.post('/PostRegistroEntrega', limitPColecciones(250, "registro_entrega"), async (req, res) =>{
+AppRegistroEntrega.post('/PostRegistroEntrega', limitPColecciones(250, "registro_entrega"), proxyPostC("registroEntPost"), async (req, res) =>{
     if(!req.rateLimit) return;
     let registro_entrega = db.collection("registro_entrega");
-
+    let data = {...req.body, _id : req.body.ID_Registro, Fecha_Entrega: new Date(req.body.Fecha_Entrega)}
     try {
-        let result = await registro_entrega.insertOne(req.body)
-        res.send(`Data Enviada correctamente`);
+        let result = await registro_entrega.insertOne(data)
+        res.status(200).send({status: 200, message: "Data enviada Correctamente"});
       } catch (error) {
-        res.send(`Error al guardar la data, _id ya se encuentra en uso`);
+        errorcontroller(error, res); 
       }
 })
 
-AppRegistroEntrega.put('/PutRegistroEntrega', limitPColecciones(250, "registro_entrega"), async (req, res) =>{
+AppRegistroEntrega.put('/PutRegistroEntrega', limitPColecciones(250, "registro_entrega"), proxyPostC("registroEntPut"), proxyQueryID, async (req, res) =>{
     if(!req.rateLimit) return;
     let registro_entrega = db.collection("registro_entrega");
+    let data = {...req.body, Fecha_Entrega: new Date(req.body.Fecha_Entrega)}
     const id = parseInt(req.query.id, 10);
     try {
         
-        let result = await registro_entrega.updateOne({ _id: id }, { $set: req.body })
+        let result = await registro_entrega.updateOne({ _id: id }, { $set: data })
         if (result.modifiedCount > 0) {
-            res.send("Documento actualizado correctamente");
+            res.status(200).send({status: 200, message: "Documento actualizado correctamente"});
         } else {
-            res.send("El documento no pudo ser encontrado o no se realizaron cambios");
+            res.status(404).send({status: 404, message: "El documento no pudo ser encontrado o no se realizaron cambios"});
+
         }
       } catch (error) {
-        res.send(`Error al Actualizar la data`);
+        errorcontroller(error, res);
       }
 })
 
-AppRegistroEntrega.delete('/DeleteRegistroEntrega', limitDColecciones(), async (req, res) =>{
+AppRegistroEntrega.delete('/DeleteRegistroEntrega', limitDColecciones(), proxyBodyID, async (req, res) =>{
     if(!req.rateLimit) return;
     let registro_entrega = db.collection("registro_entrega");
     const id = parseInt(req.body.id, 10);
     try {
         let result = await registro_entrega.deleteOne({ _id: id })
         if (result.deletedCount > 0) {
-            res.send("Documento ha sido eliminado correctamente");
-        } else {
-            res.send("El documento no pudo ser encontrado o no se elimino el documento");
-        }
-      } catch (error) {
-        res.send(`Error al Actualizar la data`);
+          res.status(200).send({status: 200, message: "Documento eliminado correctamente"});
+      } else {
+          res.status(404).send({status: 404, message: "El documento no pudo ser encontrado o no se elimino el documento"});
       }
+    } catch (error) {
+      errorcontroller(error, res);
+    }
 })
 
 export default AppRegistroEntrega;
