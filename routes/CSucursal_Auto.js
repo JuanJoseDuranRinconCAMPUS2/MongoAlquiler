4a2a624812b92172c0d3239dcc8599a12c343ca7
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-17 22:02:46 -0500

    feat: :safety_vest: Implementacion de la Validacion a los Endpoints: Sucursal_Auto
    Se modifica el archivo para incluir los Middlewares de validacion, validando la data de entrada en los metodos Post y Put y ademas en los endpoints que soliciten ID en el query o en el body se valida dicha data.

--- routes/CSucursal_Auto.js
+++ routes/CSucursal_Auto.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import {limitGColecciones, limitPColecciones, limitDColecciones} from '../middleware/limit.js';
-import bodyParser  from 'body-parser';
-import { Collection, ObjectId } from 'mongodb';
+import { proxyPostC } from "../middleware/proxyCrudColecciones.js";
+import {proxyQueryID, proxyBodyID} from "../middleware/proxyUniversal.js"
+import errorcontroller from "../middleware/ErroresMongo.js";
 import { con } from '../db/atlas.js';
 
 const AppSucursal_Auto = Router();
@@ -15,19 +16,18 @@ AppSucursal_Auto.get('/GetSucursal_Auto', limitGColecciones(), async (req, res) 
 
 })
 
-AppSucursal_Auto.post('/PostSucursal_Auto', limitPColecciones(100, "sucursal_automovil"), async (req, res) =>{
+AppSucursal_Auto.post('/PostSucursal_Auto', limitPColecciones(250, "sucursal_automovil"), proxyPostC("sucursalAutoPost"), async (req, res) =>{
     if(!req.rateLimit) return;
     let sucursal_automovil = db.collection("sucursal_automovil");
-
     try {
         let result = await sucursal_automovil.insertOne(req.body)
-        res.send(`Data Enviada correctamente`);
+        res.status(200).send({status: 200, message: "Data enviada Correctamente"});
       } catch (error) {
-        res.send(`Error al guardar la data, _id ya se encuentra en uso`);
+        errorcontroller(error, res);
       }
 })
 
-AppSucursal_Auto.put('/PutSucursal_Auto', limitPColecciones(100, "sucursal_automovil"), async (req, res) =>{
+AppSucursal_Auto.put('/PutSucursal_Auto', limitPColecciones(200, "sucursal_automovil"), proxyPostC("sucursalAutoPut"), proxyQueryID, async (req, res) =>{
     if(!req.rateLimit) return;
     let sucursal_automovil = db.collection("sucursal_automovil");
     const id = parseInt(req.query.id, 10);
@@ -35,29 +35,29 @@ AppSucursal_Auto.put('/PutSucursal_Auto', limitPColecciones(100, "sucursal_autom
         
         let result = await sucursal_automovil.updateOne({ _id: id }, { $set: req.body })
         if (result.modifiedCount > 0) {
-            res.send("Documento actualizado correctamente");
-        } else {
-            res.send("El documento no pudo ser encontrado o no se realizaron cambios");
-        }
-      } catch (error) {
-        res.send(`Error al Actualizar la data`);
+          res.status(200).send({status: 200, message: "Documento actualizado correctamente"});
+      } else {
+          res.status(404).send({status: 404, message: "El documento no pudo ser encontrado o no se realizaron cambios"});
       }
+    } catch (error) {
+      errorcontroller(error, res);
+    }
 })
 
-AppSucursal_Auto.delete('/DeleteSucursal_Auto', limitDColecciones(), async (req, res) =>{
+AppSucursal_Auto.delete('/DeleteSucursal_Auto', limitDColecciones(), proxyBodyID, async (req, res) =>{
     if(!req.rateLimit) return;
     let sucursal_automovil = db.collection("sucursal_automovil");
     const id = parseInt(req.body.id, 10);
     try {
         let result = await sucursal_automovil.deleteOne({ _id: id })
         if (result.deletedCount > 0) {
-            res.send("Documento ha sido eliminado correctamente");
-        } else {
-            res.send("El documento no pudo ser encontrado o no se elimino el documento");
-        }
-      } catch (error) {
-        res.send(`Error al Actualizar la data`);
+          res.status(200).send({status: 200, message: "Documento eliminado correctamente"});
+      } else {
+          res.status(404).send({status: 404, message: "El documento no pudo ser encontrado o no se elimino el documento"});
       }
+    } catch (error) {
+      errorcontroller(error, res);
+    }
 })
 
 export default AppSucursal_Auto;
