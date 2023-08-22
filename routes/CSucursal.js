d0f02ae6c4cf6d52e77627eb4bf9c66a2fb358e3
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-22 03:52:03 -0500

    feat: :safety_vest: Implementacion de "proxyEndpointVerify"en el endpoint: Sucursal
    Se modifica el archivo para incluir el Middleware de validacion de "Sucursal", validando que el token contenga la informacion necesaria para ingresar al endpoint. Ya sea el endpoint como tal y el rol si es necesario. Con esto se terminan todas las validaciones del JWT.

--- routes/CSucursal.js
+++ routes/CSucursal.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 import {limitGColecciones, limitPColecciones, limitDColecciones} from '../middleware/limit.js';
 import { proxyPostC } from "../middleware/proxyCrudColecciones.js";
 import {proxyQueryID, proxyBodyID} from "../middleware/proxyUniversal.js"
+import { proxyEndpointVerify } from "../middleware/proxyManejoTokens.js";
 import errorcontroller from "../middleware/ErroresMongo.js";
 import { con } from '../db/atlas.js';
 
 const AppSucursal = Router();
 let db = await con();
 
-AppSucursal.get('/GetSucursal', limitGColecciones(), async (req, res) =>{
+AppSucursal.get('/GetSucursal', limitGColecciones(), proxyEndpointVerify(0, "Sucursal"), async (req, res) =>{
     if(!req.rateLimit) return;
     let sucursal = db.collection("sucursal");
     let result = await sucursal.find({}).sort( { _id: 1 } ).toArray();
@@ -16,7 +17,7 @@ AppSucursal.get('/GetSucursal', limitGColecciones(), async (req, res) =>{
 
 })
 
-AppSucursal.post('/PostSucursal', limitPColecciones(200, "sucursal"), proxyPostC("sucursalPost"), async (req, res) =>{
+AppSucursal.post('/PostSucursal', limitPColecciones(200, "sucursal"), proxyPostC("sucursalPost"), proxyEndpointVerify(1, "Sucursal", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     let sucursal = db.collection("sucursal");
     let data = {...req.body, _id : req.body.ID_sucursal}
@@ -28,7 +29,7 @@ AppSucursal.post('/PostSucursal', limitPColecciones(200, "sucursal"), proxyPostC
       }
 })
 
-AppSucursal.put('/PutSucursal', limitPColecciones(200, "sucursal"), proxyPostC("sucursalPut"), proxyQueryID, async (req, res) =>{
+AppSucursal.put('/PutSucursal', limitPColecciones(200, "sucursal"), proxyPostC("sucursalPut"), proxyQueryID, proxyEndpointVerify(1, "Sucursal", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     let sucursal = db.collection("sucursal");
     const id = parseInt(req.query.id, 10);
@@ -45,7 +46,7 @@ AppSucursal.put('/PutSucursal', limitPColecciones(200, "sucursal"), proxyPostC("
       }
 })
 
-AppSucursal.delete('/DeleteSucursal', limitDColecciones(), proxyBodyID, async (req, res) =>{
+AppSucursal.delete('/DeleteSucursal', limitDColecciones(), proxyBodyID, proxyEndpointVerify(1, "Sucursal", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     let sucursal = db.collection("sucursal");
     const id = parseInt(req.body.id, 10);
@@ -64,7 +65,7 @@ AppSucursal.delete('/DeleteSucursal', limitDColecciones(), proxyBodyID, async (r
 //7
 //  Mostrar la cantidad total de automóviles disponibles en cada sucursal. 
 
-AppSucursal.get('/CantidadAutosSucursal', limitGColecciones(), async (req, res) =>{
+AppSucursal.get('/CantidadAutosSucursal', limitGColecciones(), proxyEndpointVerify(0, "Sucursal"), async (req, res) =>{
   if(!req.rateLimit) return;
   let sucursal = db.collection("sucursal");
   let result = await sucursal.aggregate([  
@@ -116,7 +117,7 @@ AppSucursal.get('/CantidadAutosSucursal', limitGColecciones(), async (req, res) 
 //16
 // Mostrar la cantidad total de automóviles en cada sucursal junto con su dirección.
 
-AppSucursal.get('/CantidadTotalAutosDirect', limitGColecciones(), async (req, res) =>{
+AppSucursal.get('/CantidadTotalAutosDirect', limitGColecciones(), proxyEndpointVerify(0, "Sucursal"), async (req, res) =>{
     if(!req.rateLimit) return;
     let sucursal = db.collection("sucursal");
     let result = await sucursal.aggregate([  
