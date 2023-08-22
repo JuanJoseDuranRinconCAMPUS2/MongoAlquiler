cb545600d325d36bab7f53b263437337331d23bc
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-22 03:19:58 -0500

    feat: :construction: Implementacion de "proxyEndpointVerify"en el endpoint: Colecciones
    Se modifica el archivo para incluir el Middleware de validacion de "Colecciones", validando que el token contenga la informacion necesaria para ingresar al endpoint. Ya sea el endpoint como tal y el rol si es necesario. Con esto se terminan todas las validaciones del JWT.

--- routes/CColecciones.js
+++ routes/CColecciones.js
@@ -1,20 +1,19 @@
 import { Router } from "express";
 import {limitGColecciones, limitCrearColecciones} from '../middleware/limit.js';
-import bodyParser  from 'body-parser';
-import { Collection, ObjectId } from 'mongodb';
+import { proxyEndpointVerify } from "../middleware/proxyManejoTokens.js";
 import { con } from '../db/atlas.js';
 
 const AppColecciones = Router();
 let db = await con();
 
-AppColecciones.get('/GetColecciones', limitGColecciones(), async (req, res) =>{
+AppColecciones.get('/GetColecciones', limitGColecciones(), proxyEndpointVerify(0, "Colecciones"), async (req, res) =>{
     if(!req.rateLimit) return;
     const collections = await db.listCollections().toArray();
     res.send(collections)
 
 })
 
-AppColecciones.post('/CrearColecciones', limitCrearColecciones(), async (req, res) =>{
+AppColecciones.post('/CrearColecciones', limitCrearColecciones(), proxyEndpointVerify(1, "Colecciones", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     const {Nombre} = req.body
 
@@ -28,7 +27,7 @@ AppColecciones.post('/CrearColecciones', limitCrearColecciones(), async (req, re
     }
 })
 
-AppColecciones.delete('/BorrarColeccion', limitCrearColecciones(), async (req, res) =>{
+AppColecciones.delete('/BorrarColeccion', limitCrearColecciones(), proxyEndpointVerify(1, "Colecciones", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     const {Nombre} = req.body
     try {
