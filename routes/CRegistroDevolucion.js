2d745a58801c6a5d963d47fa1bcc8d866d48a9f6
Juan Jose Duran Rincon <jduranrinconcampus@gmail.com>
2023-08-22 03:22:49 -0500

    feat: :construction: Implementacion de "proxyEndpointVerify"en el endpoint: RegistroDevolucion
    Se modifica el archivo para incluir el Middleware de validacion de "RegistroDevolucion", validando que el token contenga la informacion necesaria para ingresar al endpoint. Ya sea el endpoint como tal y el rol si es necesario. Con esto se terminan todas las validaciones del JWT.

--- routes/CRegistroDevolucion.js
+++ routes/CRegistroDevolucion.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 import {limitGColecciones, limitPColecciones, limitDColecciones} from '../middleware/limit.js';
 import { proxyPostC } from "../middleware/proxyCrudColecciones.js";
 import {proxyQueryID, proxyBodyID} from "../middleware/proxyUniversal.js"
+import { proxyEndpointVerify } from "../middleware/proxyManejoTokens.js";
 import errorcontroller from "../middleware/ErroresMongo.js";
 import { con } from '../db/atlas.js';
 
 const AppRegistroDevolucion = Router();
 let db = await con();
 
-AppRegistroDevolucion.get('/GetRegistroDevolucion', limitGColecciones(), async (req, res) =>{
+AppRegistroDevolucion.get('/GetRegistroDevolucion', limitGColecciones(), proxyEndpointVerify(0, "RegistroDevolucion"), async (req, res) =>{
     if(!req.rateLimit) return;
     let registro_devolucion = db.collection("registro_devolucion");
     let result = await registro_devolucion.find({}).sort( { _id: 1 } ).toArray();
@@ -16,7 +17,7 @@ AppRegistroDevolucion.get('/GetRegistroDevolucion', limitGColecciones(), async (
 
 })
 
-AppRegistroDevolucion.post('/PostRegistroDevolucion', limitPColecciones(320, "registro_devolucion"), proxyPostC("registroDevPost"), async (req, res) =>{
+AppRegistroDevolucion.post('/PostRegistroDevolucion', limitPColecciones(320, "registro_devolucion"), proxyPostC("registroDevPost"), proxyEndpointVerify(1, "RegistroDevolucion", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     let registro_devolucion = db.collection("registro_devolucion");
     let data = {...req.body, _id : req.body.ID_Registro, Fecha_Devolucion: new Date(req.body.Fecha_Devolucion)}
@@ -29,7 +30,7 @@ AppRegistroDevolucion.post('/PostRegistroDevolucion', limitPColecciones(320, "re
       }
 })
 
-AppRegistroDevolucion.put('/PutRegistroDevolucion', limitPColecciones(300, "registro_devolucion"), proxyPostC("registroDevPut"), proxyQueryID, async (req, res) =>{
+AppRegistroDevolucion.put('/PutRegistroDevolucion', limitPColecciones(300, "registro_devolucion"), proxyPostC("registroDevPut"), proxyQueryID, proxyEndpointVerify(1, "RegistroDevolucion", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     let registro_devolucion = db.collection("registro_devolucion");
     let data = {...req.body, Fecha_Devolucion: new Date(req.body.Fecha_Devolucion)}
@@ -48,7 +49,7 @@ AppRegistroDevolucion.put('/PutRegistroDevolucion', limitPColecciones(300, "regi
       }
 })
 
-AppRegistroDevolucion.delete('/DeleteRegistroDevolucion', limitDColecciones(), proxyBodyID, async (req, res) =>{
+AppRegistroDevolucion.delete('/DeleteRegistroDevolucion', limitDColecciones(), proxyBodyID, proxyEndpointVerify(1, "RegistroDevolucion", "Admin"), async (req, res) =>{
     if(!req.rateLimit) return;
     let registro_devolucion = db.collection("registro_devolucion");
     const id = parseInt(req.body.id, 10);
